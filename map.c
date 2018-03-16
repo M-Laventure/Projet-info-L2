b343fe36ec5aa409600ac12ba3b8f687b9184881
daurelien <36164922+daurelien@users.noreply.github.com>
2018-03-16 10:42:29 +0100

    Update map.c

--- map.c
+++ map.c
@@ -13,15 +13,15 @@ int main (int argc, char ** argv){
 
 	srand(time(NULL));			/*initialisation du random*/
 	int entered_size=0;
-	int entered_units=0;
+	int credit =0;
 	int ** MainMap=NULL ; /**pointeur sur notre carte créée dans le tas**/;
 
 	do{
-	printf("Entrez le nombre d'unités(pas plus de 30)\n");
-	scanf("%d",&entered_units);
-	}while(entered_units>=30&&entered_units<1);  /*****Entrez nombre d'unités*****/
+	printf("Combien de crédits souhaitez-vous ?(pas plus de 100)\n");
+	scanf("%d",&credit);
+	}while(credit>=100 && credit<1);  /*****Entrez nombre d'unités*****/
 
-	entered_size= unitTosize(&entered_units); /*****Convertir NOMBRE D'unités EN TAILLE*********/
+	entered_size= unitTosize(&credit); /*****Convertir NOMBRE D'unités EN TAILLE*********/
 	printf("%d \n",entered_size);
 	MainMap = Map_Maker1(&entered_size);
 	AfficherMap(MainMap,&entered_size); /****MAP VIDE*****/
@@ -32,7 +32,7 @@ int main (int argc, char ** argv){
 
 	printf("Press any key to continue...\n");
 	scanf("%d",&v);
-	placement_auto1(MainMap,&entered_size,&entered_units,'d');
+	placement_auto1(MainMap,&entered_size,&credit,'d');
 	while(v>0){
 	AfficherMap(MainMap,&entered_size);
 	printf("Press any key to continue...\n");
