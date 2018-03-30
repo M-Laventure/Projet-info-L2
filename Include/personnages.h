26b048062adb10a99eddaab1b99c742f62358fc0
Bounou <35834127+Vounou@users.noreply.github.com>
2018-03-30 11:04:21 +0200

    Update personnages.h

--- Include/personnages.h
+++ Include/personnages.h
@@ -1,28 +1,17 @@
 #include <stdlib.h> 
 #include <stdio.h>
 
-	
-/*
-typedef enum {witcher, mage, scoiatel, nains, spectre, wyvern, goule, leshen , cyclope, doppler} id_unite;
-*/	
-typedef struct{
-	
-	int id_type; //0 les deux, 1 humanoide, 2 monstre
-	int bonus;
-	int faiblesse;
-} type;
+//typedef enum {witcher, mage, scoiatel, nains, spectre, wyvern, goule, leshen , cyclope, doppler} id_classe;
 
- 
- 
+
 //Chaque unité possède une certaine portée d'attaque verticale et horizontale
 
 typedef struct {
  int hor;
  int vert;
 } range;
-/*typedef enum {
-		aveuglement, poison, immobilisation, etourdissement
-}*/
+
+
 typedef struct
 	{
 		int credit;
@@ -31,8 +20,6 @@ typedef struct
 		int def;
 		range portee_attaque;
 		int deplacement;
-		type character;
-		/*alteration capacite_spe;*/
 	}	unite_stats;
 
 typedef struct{
@@ -45,12 +32,8 @@ typedef struct{
 	 int id_joueur;
 	 int id_unite ;
 	 int id_classe;
-	// stats caracteristiques;//
+	 int type;      //0 les deux, 1 humanoide, 2 monstre
 	 char nom[20];
   	 unite_stats stats;
 	 unite_coord coord;
-	
-	 } unite_s ;
-	 
-	 
-
+} unite_s ;
