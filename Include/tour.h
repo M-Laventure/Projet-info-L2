8427a470a10e938564fc7bd9a0f221368a496bd0
Marin_Lav <35834127+M-Laventure@users.noreply.github.com>
2018-04-03 12:30:54 +0000

    Create tour.h

---
+++ Include/tour.h
@@ -0,0 +1,19 @@
+void tour_unite(unite_s *tab_ordrejeu, int id_unite, int **Map,int taille_map);
+
+
+extern void tour_unite(unite_s *tab_ordrejeu, int id_unite, int **Map,int taille_map){
+	int choix ;
+	printf ("Choix: 1 -attaquer, 2 -deplacer,autre -passer\n");
+	scanf("%i",&choix);
+	if(choix == 1){
+		/*attaquer(tab_ordrejeu, id_unite);*/
+	}
+	if(choix == 2){
+		deplacer(tab_ordrejeu, id_unite, Map,taille_map); 
+		printf(" Choix: 1 -attaquer, autre -passer\n");
+		scanf("%i",&choix);
+		if (choix==1){
+			/*attaquer(tab_ordrejeu,id_unite);*/
+		}
+	}
+}
