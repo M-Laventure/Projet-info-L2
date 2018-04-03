50e77d10749e35a9683ee1258203daac2f5f7f31
Marin_Lav <35834127+M-Laventure@users.noreply.github.com>
2018-04-03 11:50:06 +0000

    Update meo_attaque.h

--- Include/meo_attaque.h
+++ Include/meo_attaque.h
@@ -1,1 +1,8 @@
+/* Mise en oeuvre attaque */
+void attaquer(unite_s *tab_ordrejeu, int id_unite, int **Map);
+void calcul_dmg(unite_s *tab_ordrejeu, int id_unite, int id_cible);
+int block(unite_s *tab_ordrejeu, int id_cible);
+int trajectoire_bloque(int **Map, unite_s *tab_ordrejeu, int id_cible, int id_unite);
+int est_a_portee(unite_s *tab_ordrejeu, int **Map, int id_unite, int id_cible);
+int est_vulnerable(unite_s *tab_ordrejeu, int id_unite, int id_cible);
 
