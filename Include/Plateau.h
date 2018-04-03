11346d6f297758580c1e4e43027ab2ac5c823356
Marin_Lav <35834127+M-Laventure@users.noreply.github.com>
2018-04-03 11:56:29 +0000

    Update Plateau.h

--- Include/Plateau.h
+++ Include/Plateau.h
@@ -1,1 +1,9 @@
+/* Génération matrice plateau/obstacles */
 
+
+
+
+int** Map_Maker1(int * ); //fonction qui initialise la carte
+void AfficherMap(int **, int * );//fonction qui affiche la carte
+int  unitTosize(int *);//fonction redéfinie la taille de la map
+void init_obstacle(int **, int *);
