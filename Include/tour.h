f17db707cde2c50291c3e928e1472f62936ac4de
Marin_Lav <35834127+M-Laventure@users.noreply.github.com>
2018-04-03 12:32:06 +0000

    Update tour.h

--- Include/tour.h
+++ Include/tour.h
@@ -6,14 +6,14 @@ extern void tour_unite(unite_s *tab_ordrejeu, int id_unite, int **Map,int taille
 	printf ("Choix: 1 -attaquer, 2 -deplacer,autre -passer\n");
 	scanf("%i",&choix);
 	if(choix == 1){
-		/*attaquer(tab_ordrejeu, id_unite);*/
+		attaquer(tab_ordrejeu, id_unite, Map);
 	}
 	if(choix == 2){
 		deplacer(tab_ordrejeu, id_unite, Map,taille_map); 
 		printf(" Choix: 1 -attaquer, autre -passer\n");
 		scanf("%i",&choix);
 		if (choix==1){
-			/*attaquer(tab_ordrejeu,id_unite);*/
+			attaquer(tab_ordrejeu,id_unite, Map);
 		}
 	}
 }
